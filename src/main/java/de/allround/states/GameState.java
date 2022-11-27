d8dcf6d6c2fdea37db11ce5b6ebe6e6fe5386fc2
AllroundYT <80672930+AllroundYT@users.noreply.github.com>
2022-11-27 22:59:42 +0100

    GameState System hinzugefügt

--- src/main/java/de/allround/states/GameState.java
+++ src/main/java/de/allround/states/GameState.java
@@ -2,23 +2,45 @@ package de.allround.states;
 
 import de.allround.states.events.GameStateStartEvent;
 import de.allround.states.events.GameStateStopEvent;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.bukkit.Bukkit;
 
-public interface GameState {
-    void onStart();
-    void onStop();
+@RequiredArgsConstructor
+@Getter
+public abstract class GameState {
+    private final String name;
+    private boolean running;
 
-    default void start(){
+    public boolean isRunning() {
+        return running;
+    }
+
+    /**
+     * Executed when this GameState starts.
+     */
+    public abstract void onStart();
+
+    /**
+     * Executed when this GameState stopps.
+     */
+    public abstract void onStop();
+
+    public void start() {
+        if (isRunning()) return;
         GameStateStartEvent gameStateStartEvent = new GameStateStartEvent(this);
         Bukkit.getPluginManager().callEvent(gameStateStartEvent);
         if (gameStateStartEvent.isCancelled()) return;
         onStart();
+        this.running = true;
     }
 
-    default void stop(){
+    public void stop() {
+        if (!isRunning()) return;
         GameStateStopEvent gameStateStopEvent = new GameStateStopEvent(this);
         Bukkit.getPluginManager().callEvent(gameStateStopEvent);
         if (gameStateStopEvent.isCancelled()) return;
         onStop();
+        this.running = false;
     }
 }
--- src/main/java/de/allround/states/GameStateManager.java
+++ src/main/java/de/allround/states/GameStateManager.java
@@ -1,4 +1,38 @@
 package de.allround.states;
 
-public class GameStateManager { //TODO
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class GameStateManager {
+    private final List<GameState> gameStates;
+    private GameState runningState;
+    public GameStateManager() {
+        this.gameStates = new ArrayList<>();
+    }
+
+    public List<GameState> getGameStates() {
+        return gameStates;
+    }
+
+    public void registerGameStates(GameState... gameStates){
+        this.gameStates.addAll(Arrays.asList(gameStates));
+    }
+
+    public Optional<GameState> getGameState(String name){
+        return getGameStates().stream().filter(gameState -> gameState.getName().equals(name)).findFirst();
+    }
+
+    public GameState getRunningState() {
+        return runningState;
+    }
+
+    public void startGameState(String state){
+        Optional<GameState> gameState = getGameState(state);
+        if (gameState.isEmpty()) return;
+        this.runningState.stop();
+        this.runningState = gameState.get();
+        this.runningState.start();
+    }
 }
