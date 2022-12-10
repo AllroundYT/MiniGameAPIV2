85f5c517b39f14f669c9bdddd734e18be15a1c4f
AllroundYT <80672930+AllroundYT@users.noreply.github.com>
2022-12-10 15:24:54 +0100

    Minigame JavaDoc added

--- src/main/java/de/allround/Minigame.java
+++ src/main/java/de/allround/Minigame.java
@@ -2,53 +2,88 @@ package de.allround;
 
 import de.allround.event.DefaultListener;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
-import java.lang.reflect.InvocationTargetException;
-
-public abstract class Minigame{
+/**
+ * Simple self explaining class for creating a minigame on a PaperSpigot Minecraft server.
+ */
+public abstract class Minigame {
 
     public static Minigame instance;
     private final IGameManager IGameManager;
     private final Plugin plugin;
 
-    public Plugin getPlugin() {
-        return plugin;
-    }
-
-    public Minigame(Plugin plugin, Class<? extends IGameManager> iGameManagerClass) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    /**
+     * @param plugin            The {@link Plugin} this minigame is referred to.
+     * @param iGameManagerClass The {@link IGameManager} class which should be used by this minigame instance.
+     * @throws Exception When creating a new instance of {@param iGameManagerClass} isn't possible.
+     */
+    public Minigame(Plugin plugin, @NotNull Class<? extends IGameManager> iGameManagerClass) throws Exception {
         Minigame.instance = this;
         this.plugin = plugin;
         this.IGameManager = iGameManagerClass.getDeclaredConstructor().newInstance();
     }
 
-    public static Minigame getInstance(Class<? extends Minigame> mainClass) {
-        return mainClass.cast(instance);
-    }
-
+    /**
+     * Returns this minigame instance.
+     *
+     * @return This minigame instance.
+     */
     public static Minigame getInstance() {
         return instance;
     }
 
+    /**
+     * Returns the {@link Plugin} this minigame is referred to.
+     *
+     * @return the {@link Plugin} this minigame is referred to.
+     */
+    public Plugin getPlugin() {
+        return plugin;
+    }
+
+    /**
+     * Returns this minigame instance's {@link IGameManager} instance.
+     *
+     * @return This minigame instance's {@link IGameManager} instance.
+     */
     public IGameManager getGameManager() {
         return IGameManager;
     }
 
+    /**
+     * Executed on minigame initialization.
+     */
     protected abstract void onInit();
 
+    /**
+     * Executed on minigame start.
+     */
     protected abstract void onStart();
 
+    /**
+     * Executed on minigame stop.
+     */
     protected abstract void onStop();
 
+    /**
+     * Loads the minigame.
+     */
     public final void load() {
         onInit();
     }
 
+    /**
+     * Starts the minigame.
+     */
     public final void enable() {
         new DefaultListener().register();
         onStart();
     }
 
+    /**
+     * Stopps the minigame.
+     */
     public final void disable() {
         onStop();
     }
